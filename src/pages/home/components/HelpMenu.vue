5ee0597b26f4ba4953ec09a8daef075366138df7
huangdd <424402750@qq.com>
2020-08-16 13:28:08 +0800

    up

--- src/pages/home/components/HelpMenu.vue
+++ src/pages/home/components/HelpMenu.vue
@@ -1,49 +1,72 @@
 <template>
-  <div style="padding:40px;font-szie:14px">
-    <p style="color:#236aeb"># 自动化平台帮助</p>
-    <h3 style="color:#236aeb">## API 调试</h3>
-    <p>1. 当前url > 环境url > 配置rul：</p>
-    <p>示例：/api/longin</p>
-    <p>自动补全请求地址后:<span style="color:#236aeb">www.baidu.com/api/longin</span></p>
-    <p>2. 可配置：</p>
-    <p>全局变量》环境变量》配置变量</p>
-    <p>3. Header：</p>
-    <p>一行一行添加</p>
-    <p>4. Requesr：</p>
-    <p>变量参数化 "userName": "$name"，必须为"$name"</p>
-    <p>可："$name123$pass" name与pass均为变量</p>
-    <p>如果变量不存在，直接失败报错</p>
-    <p>5. Extract:</p>
-    <p>后续补充</p>
-    <p>6. Validate:</p>
-    <p>后续补充</p>
-    <p>7. Variables:</p>
-    <p>后续补充</p>
-    <p>8. Hooks:</p>
-    <p>后续补充</p>
-    <h3  style="color:#236aeb">## API库</h3>
-    <p>1. 分组,参照现有模块进行分组，根节点与子节点</p>
-    <p>2. 每个api后面有：编辑，运行，复制，删除按钮</p>
-    <p>3. 复制直接复制相同内容，但是需要新增名字</p>
-    <h3  style="color:#236aeb">## 测试用例</h3>
-    <p>1. 分为冒烟，集成，监控用例</p>
-    <p>2. 模式：分组，添加用例，用例添加接口组合，组成业务逻辑</p>
-    <p>3. 添加用例，需要从api库中api从左边移动到右边用例框，然后新增名字保存</p>
-    <p>业务逻辑自行移动，上下移动代表执行顺序</p>
-    <p>保存用例的时候可以选择配置变量，进行保存</p>
-    <p>4. 如果api库中api更新，需要更新用例api,可点击用例后面同步按钮</p>
-    <p>5. 用例可以进行运行，可以选择同步执行或者异步执行</p>
-    <h4  style="color:#236aeb">## 配置管理-全局变量-域名环境</h4>
-    <p>1. 均有地址设置，可以开关</p>
-    <p>2. 变量名称，选择类型，变量值，备注</p>
-    <h4  style="color:#236aeb">## 历史报告</h4>
-    <p>1. 可以查看，下载，删除</p>
-    <h4  style="color:#236aeb">## 异步回执</h4>
-    <p>1. 显示执行状态，成功，失败，与结束时间</p>
-    <p>2. 失败的异步，可以鼠标移动到traceback上面查看报错</p>
-    <h3  style="color:#236aeb">### 其他注意点：</h3>
-    <p>- 后台日志未分页</p>
-    <p>- 测试数据模块未调通</p>
+  <div style="padding:40px;box-sizing:border-box;font-szie:14px;height:100vh;overflow:fidden">
+    <div style="overflow-y:scroll;height:100%;">
+      # 自动化平台帮助
+      <br />
+      <br />## API 调试
+      <br />```
+      <br />1. 当前url > 环境url > 配置rul：
+      <br />示例：/api/longin
+      <br />自动补全请求地址后:www.baidu.com/api/longin
+      <br />2. 可配置：
+      <br />全局变量》环境变量》配置变量
+      <br />3. Header：
+      <br />一行一行添加
+      <br />4. Requesr：
+      <br />变量参数化 "userName": "$name"，必须为"$name"
+      <br />可："$name123$pass" name与pass均为变量
+      <br />如果变量不存在，直接失败报错
+      <br />5. Extract:
+      <br />抽取接口返回值 变量：userKey 提取式：content.data.userKey 下个接口直接调用"$userKey"
+      <br />6. Validate:
+      <br />校验 extract 提取,示例：eq content.code integer 200
+      <br />7. Variables:
+      <br />后续补充
+      <br />8. Hooks:
+      <br />后续补充
+      <br />9. Save保存选择已有的api库分组进行保存，Send运行直接运行
+      <br />```
+      <br />
+      <br />## API库
+      <br />```
+      <br />1. 分组,参照现有模块进行分组，根节点与子节点
+      <br />2. 每个api后面有：编辑，运行，复制，删除按钮
+      <br />3. 复制直接复制相同内容，但是需要新增名字
+      <br />```
+      <br />
+      <br />## 测试用例
+      <br />```
+      <br />1. 分为冒烟，集成，监控用例
+      <br />2. 模式：分组，添加用例，用例添加接口组合，组成业务逻辑
+      <br />3. 添加用例，需要从api库中api从左边移动到右边用例框，然后新增名字保存
+      <br />业务逻辑自行移动，上下移动代表执行顺序
+      <br />保存用例的时候可以选择配置变量，进行保存
+      <br />4. 如果api库中api更新，需要更新用例api,可点击用例后面同步按钮
+      <br />5. 用例可以进行运行，可以选择同步执行或者异步执行
+      <br />```
+      <br />
+      <br />## 配置管理-全局变量-域名环境
+      <br />```
+      <br />1. 均有地址设置，可以开关
+      <br />2. 变量名称，选择类型，变量值，备注
+      <br />```
+      <br />## 历史报告
+      <br />```
+      <br />1. 可以查看，下载，删除
+      <br />```
+      <br />## 异步回执
+      <br />```
+      <br />1. 显示执行状态，成功，失败，与结束时间
+      <br />2. 失败的异步，可以鼠标移动到traceback上面查看报错
+      <br />```
+      <br />
+      <br />
+      <br />### 其他注意点：
+      <br />- 后台日志未分页
+      <br />- 测试数据模块未调通
+      <br />
+      <br />
+    </div>
   </div>
 </template>
 
